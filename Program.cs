dd6bd0428ac2381dcb41b17296d2c6b1af1aac18
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-02-22 17:15:42 +0530

    ADDED: Max method to take more then three parameters

--- Program.cs
+++ Program.cs
@@ -9,22 +9,6 @@ namespace FindMaximum
         {
             Console.WriteLine("Welcome to the Maximum Problem Using Generics");
 
-            //Console.WriteLine("The test cases to find the maximum value among three values");
-
-            /*Console.WriteLine("\nTest case 1: ");
-            int val1 = GenericClass<int>.MaxValue(100, 50, 75);
-            Console.WriteLine($"{val1} is the greatest among three number");
-            Console.WriteLine("\n--------------");
-
-            Console.WriteLine("Test case 2: ");
-            float val2 = GenericClass<float>.MaxValue(4.5f, 6.5f, 2.5f);
-            Console.WriteLine($"{val2} is the greatest among three number");
-            Console.WriteLine("\n--------------");
-
-            Console.WriteLine("Test case 3: ");
-            string val3 = GenericClass<string>.MaxValue("Peach", "Apple", "Banana");
-            Console.WriteLine($"{val3} is the greatest among three number");*/
-
             int[] arr = { 100,200,300,500,400 };
             ExtendMethod<int> generic = new ExtendMethod<int>(arr);
             Console.WriteLine("Maximum value amongst the provided numbers is : {0}", generic.MaxValue(arr));
