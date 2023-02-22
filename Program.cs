5df3d43b3f770341b4fd0dcb7e2a3d70e90761dd
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-22 16:19:49 +0530

    ADDED: Find Maximum Value Using Generic Method

--- Program.cs
+++ Program.cs
@@ -8,21 +8,21 @@ namespace FindMaximum
         {
             Console.WriteLine("Welcome to the Maximum Problem Using Generics");
 
-            Console.WriteLine("The test cases to find the maximum string among three strings");
+            Console.WriteLine("The test cases to find the maximum value among three values");
 
             Console.WriteLine("\nTest case 1: ");
-            string String1 = MaximumString.MaxString("Peach", "Apple", "Banana");
-            Console.WriteLine($"{String1} is the greatest among three number");
+            int val1 = GenericMethod.MaxValue<int>(100, 50, 75);
+            Console.WriteLine($"{val1} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 2: ");
-            string String2 = MaximumString.MaxString("Apple", "Mango", "Limon");
-            Console.WriteLine($"{String2} is the greatest among three number");
+            float val2 = GenericMethod.MaxValue<float>(4.5f, 6.5f, 2.5f);
+            Console.WriteLine($"{val2} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 3: ");
-            string String3 = MaximumString.MaxString("Limon", "Mango", "Orange");
-            Console.WriteLine($"{String3} is the greatest among three number");
+            string val3 = GenericMethod.MaxValue<string>("Peach", "Apple", "Orange");
+            Console.WriteLine($"{val3} is the greatest among three number");
         }
     }
 }
