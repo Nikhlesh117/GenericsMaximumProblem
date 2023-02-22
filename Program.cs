f01f3fb20e586670e5d10ef475ef01d539314494
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-22 14:19:33 +0530

    ADDED: Find Maximum Integer Number

--- Program.cs
+++ Program.cs
@@ -5,6 +5,22 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Maximum Problem Using Generics");
+
+            Console.WriteLine("The test cases to find the maximum numbe among three numbers");
+
+            Console.WriteLine("\nTest case 1: ");
+            int max1 = MaximumInteger.MaxInteger(100, 50, 75);
+            Console.WriteLine($"{max1} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 2: ");
+            int max2 = MaximumInteger.MaxInteger(40, 60, 20);
+            Console.WriteLine($"{max2} is the greatest among three number");
+            Console.WriteLine("\n--------------");
+
+            Console.WriteLine("Test case 3: ");
+            int max3 = MaximumInteger.MaxInteger(10, 35, 50);
+            Console.WriteLine($"{max3} is the greatest among three number");
         }
     }
 }
