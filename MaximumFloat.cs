2726cdbdc1c1650e5c7374cd96e5279fa4e8aa3c
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-22 14:32:21 +0530

    ADDED: Find Maximum Float Number

---
+++ MaximumFloat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class MaximumFloat
+    {
+        public static float MaxFloat(float firstValue, float secondValue, float thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0 ||
+                firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0 ||
+                secondValue.CompareTo(firstValue) < 0 && secondValue.CompareTo(thirdValue) < 0)
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(firstValue) >= 0 ||
+                thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            return firstValue;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FindMaximum
+﻿using System.Runtime.InteropServices;
+
+namespace FindMaximum
 {
     class Program
     {
@@ -6,20 +8,20 @@
         {
             Console.WriteLine("Welcome to the Maximum Problem Using Generics");
 
-            Console.WriteLine("The test cases to find the maximum numbe among three numbers");
+            Console.WriteLine("The test cases to find the maximum number among three numbers");
 
             Console.WriteLine("\nTest case 1: ");
-            int num1 = MaximumInteger.MaxInteger(100, 50, 75);
+            float num1 = MaximumFloat.MaxFloat(10.5f, 5.5f, 7.5f);
             Console.WriteLine($"{num1} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 2: ");
-            int num2 = MaximumInteger.MaxInteger(40, 60, 20);
+            float num2 = MaximumFloat.MaxFloat(4.5f, 6.5f, 2.5f);
             Console.WriteLine($"{num2} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 3: ");
-            int num3 = MaximumInteger.MaxInteger(10, 35, 50);
+            float num3 = MaximumFloat.MaxFloat(1.5f, 3.5f, 5.5f);
             Console.WriteLine($"{num3} is the greatest among three number");
         }
     }
