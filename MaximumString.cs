c19e23e2aa90465ba6c7e34ff757066d3952ea98
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-02-22 14:46:35 +0530

    ADDED: Find Maximum String

---
+++ MaximumString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class MaximumString
+    {
+        public static string MaxString(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0 ||
+                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0 ||
+                second.CompareTo(first) < 0 && second.CompareTo(third) < 0)
+            {
+                return second;
+            }
+
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0 ||
+                third.CompareTo(second) >= 0 && third.CompareTo(first) >= 0 ||
+                third.CompareTo(second) > 0 && third.CompareTo(first) >= 0)
+            {
+                return third;
+            }
+
+            return first;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,21 +8,22 @@ namespace FindMaximum
         {
             Console.WriteLine("Welcome to the Maximum Problem Using Generics");
 
-            Console.WriteLine("The test cases to find the maximum number among three numbers");
+            Console.WriteLine("The test cases to find the maximum string among three strings");
 
             Console.WriteLine("\nTest case 1: ");
-            float num1 = MaximumFloat.MaxFloat(10.5f, 5.5f, 7.5f);
-            Console.WriteLine($"{num1} is the greatest among three number");
+            string String1 = MaximumString.MaxString("Peach", "Apple", "Banana");
+            Console.WriteLine($"{String1} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 2: ");
-            float num2 = MaximumFloat.MaxFloat(4.5f, 6.5f, 2.5f);
-            Console.WriteLine($"{num2} is the greatest among three number");
+            string String2 = MaximumString.MaxString("Apple", "Mango", "Limon");
+            Console.WriteLine($"{String2} is the greatest among three number");
             Console.WriteLine("\n--------------");
 
             Console.WriteLine("Test case 3: ");
-            float num3 = MaximumFloat.MaxFloat(1.5f, 3.5f, 5.5f);
-            Console.WriteLine($"{num3} is the greatest among three number");
+            string String3 = MaximumString.MaxString("Limon", "Mango", "Orange");
+            Console.WriteLine($"{String3} is the greatest among three number");
         }
     }
 }
+    
